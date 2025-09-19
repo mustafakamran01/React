36c8d43fe5892a2c8c7749a5391f401369174022
mustafakamran01 <kamran000mustafa@gmail.com>
2025-09-19 18:59:54 +0530

    interview question

--- 02_counter/src/App.jsx
+++ 02_counter/src/App.jsx
@@ -6,11 +6,28 @@ import './App.css'
 
 function App() {
   
-  const [counter, setCounter] = useState(15)    
+  const [counter, setCounter] = useState(8)    
 
   const addValue = () => {
     if(counter < 20){
-      setCounter(counter + 1)
+      // setCounter(counter + 1)
+      // setCounter(counter + 1)
+      // setCounter(counter + 1)
+      // setCounter(counter + 1)
+      /*
+        In this scenario, it will increase the value only by 1 not by 4, because
+        react follow fibre algo, in which these operations will bundled together and then go for compilation
+      */
+
+      setCounter((prevCounter) => prevCounter + 1)
+      setCounter((prevCounter) => prevCounter + 1)
+      setCounter((prevCounter) => prevCounter + 1)
+      setCounter((prevCounter) => prevCounter + 1)
+      /*
+        In this scenario, the value will be increased by 4, because it firstly takes the previous value and then perform the operation,
+        after that it goes to the next method
+      */
+
     }
     // console.log(counter, Math.random());
     
@@ -21,7 +38,7 @@ function App() {
       setCounter(counter - 1)
     }
   }
-
+ 
   return (
     <>
       <h1>Counter number</h1>
