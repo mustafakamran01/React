6db6c1d0ca1f9e4257a6bba1837eeb035bdee05b
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-10 11:39:49 +0530

    Tailwind Props

--- 03_tailwindProps/src/App.jsx
+++ 03_tailwindProps/src/App.jsx
@@ -2,40 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-// import Card from './components/Card'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+
+  let myObj = {
+    email: "k@apple.com"
+  }
+
+  let myArr = [1, 2, 3, 4, 5]
 
   return (
   <>
     <h1 className='bg-green-400 text-black p-4 rounded-3xl mb-4'>Tailwind test</h1>
-
-    <div className="max-w-xs rounded-md shadow-md bg-black">
-      <img
-        src="https://picsum.photos/301" 
-        alt=""
-        className="object-cover object-center w-full rounded-t-md h-72 bg-gray-500"
-      />
-      <div className="flex flex-col justify-between p-6 space-y-8">
-        <div className="space-y-2">
-          <h2 className="text-3xl font-semibold tracking-wide text">Lorem</h2>
-          <p className="text-gray-400">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio
-            tempora ipsum soluta amet corporis accusantium aliquid consectetur
-            eaque!
-          </p>
-        </div>
-        <button
-          type="button"
-          className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-gray-800 text-gray-200"
-        >
-          Read more
-        </button>
-      </div>
-    </div>
-
+    <Card userName = "Mustafa"/>
+    <Card userName = "Kamran"/>
+    {/* <Card name = "Mustafa Kamran" obj = {myObj} arr = {myArr}/> */}
   </>
+  /* This is how we can transfer props(properties) from one component to another */
   )
 }
 
