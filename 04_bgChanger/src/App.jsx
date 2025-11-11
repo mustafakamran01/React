e65eaf536d4e694ac289d0dea33782d31d181b1c
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-11 18:20:41 +0530

    Re-do project

--- 04_bgChanger/src/App.jsx
+++ 04_bgChanger/src/App.jsx
@@ -4,7 +4,43 @@ import { useState } from 'react'
 
 function App() {
 
-  
+  const [color, setColor] = useState("olive")
+
+
+  return (
+
+    <div className='w-full h-screen' style={{backgroundColor: color}}
+    >
+      <div className='fixed flex flex-wrap bottom-8 justify-center inset-x-0 px-2'
+      >
+        <div className='flex flex-wrap bg-white rounded-4xl gap-3 justify-center px-3 py-2 shadow-none'
+        >
+          <button onClick={ () => setColor("red")}
+          className='outline-none bg-red-500 text-white rounded-full px-6 py-2'
+          >Red</button>
+
+          <button onClick={ () => setColor("green")}
+          className='outline-none, bg-green-600 text-white rounded-full px-4'
+          >Green</button>
+
+          <button onClick={ () => setColor("blue")}
+          className='outline-none bg-blue-600 text-white rounded-full px-5'
+          >Blue</button>
+
+          <button onClick={ () => setColor("pink")}
+          className='outline-none bg-pink-500 text-white rounded-full px-5'
+          >Pink</button>
+
+          <button onClick={ () => setColor("purple")}
+          className='outline-none bg-purple-700 text-white rounded-full px-4'
+          >Purple</button>
+
+        </div>
+
+      </div>
+
+    </div>
+  )
 }
 
 export default App
