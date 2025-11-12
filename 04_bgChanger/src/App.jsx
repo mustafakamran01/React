55f8832c493d1d586df59fe75b8ad9896f5d0dff
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-12 10:31:41 +0530

    Project Revision

--- 04_bgChanger/src/App.jsx
+++ 04_bgChanger/src/App.jsx
@@ -9,41 +9,43 @@ function App() {
 
   return (
 
-    <div className='w-full h-screen' style={{backgroundColor: color}}
-    >
-      <div className='fixed flex flex-wrap bottom-8 justify-center inset-x-0 px-2'
-      >
-        <div className='flex flex-wrap bg-white rounded-4xl gap-3 justify-center px-3 py-2 shadow-none'
-        >
-          <button onClick={ () => setColor("red")}
-          className='outline-none bg-red-500 text-white rounded-full px-6 py-2'
-          >Red</button>
+    // <div className='w-full h-screen' style={{backgroundColor: color}}
+    // >
+    //   <div className='fixed flex flex-wrap bottom-8 justify-center inset-x-0 px-2'
+    //   >
+    //     <div className='flex flex-wrap bg-white rounded-4xl gap-3 justify-center px-3 py-2 shadow-none'
+    //     >
+    //       <button onClick={ () => setColor("red")}
+    //       className='outline-none bg-red-500 text-white rounded-full px-6 py-2'
+    //       >Red</button>
 
-          <button onClick={ () => setColor("green")}
-          className='outline-none, bg-green-600 text-white rounded-full px-4 py-2'
-          >Green</button>
+    //       <button onClick={ () => setColor("green")}
+    //       className='outline-none, bg-green-600 text-white rounded-full px-4 py-2'
+    //       >Green</button>
 
-          <button onClick={ () => setColor("blue")}
-          className='outline-none bg-blue-600 text-white rounded-full px-5 py-2'
-          >Blue</button>
+    //       <button onClick={ () => setColor("blue")}
+    //       className='outline-none bg-blue-600 text-white rounded-full px-5 py-2'
+    //       >Blue</button>
 
-          <button onClick={ () => setColor("pink")}
-          className='outline-none bg-pink-500 text-white rounded-full px-5 py-2'
-          >Pink</button>
+    //       <button onClick={ () => setColor("pink")}
+    //       className='outline-none bg-pink-500 text-white rounded-full px-5 py-2'
+    //       >Pink</button>
 
-          <button onClick={ () => setColor("purple")}
-          className='outline-none bg-purple-700 text-white rounded-full px-4 py-2'
-          >Purple</button>
+    //       <button onClick={ () => setColor("purple")}
+    //       className='outline-none bg-purple-700 text-white rounded-full px-4 py-2'
+    //       >Purple</button>
 
-          <button onClick={ () => setColor("gray")}
-          className='outline-none bg-gray-500 text-white rounded-full px-5 py-2'
-          >Gray</button>
+    //       <button onClick={ () => setColor("gray")}
+    //       className='outline-none bg-gray-500 text-white rounded-full px-5 py-2'
+    //       >Gray</button>
 
-        </div>
+    //     </div>
 
-      </div>
+    //   </div>
 
-    </div>
+    // </div>
+
+    
   )
 }
 
