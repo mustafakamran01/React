1842bd1b7f7b0f394877c4fe7bf438604a4e783f
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-13 11:39:44 +0530

    Setting up HTML code for project

--- 05_passwordGenerator/src/App.jsx
+++ 05_passwordGenerator/src/App.jsx
@@ -1,34 +1,57 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="max-w-xl mx-auto p-6 bg-white rounded-2xl shadow-lg justify-center inset-x-0">
+      <h3 className="text-lg font-semibold mb-4">Input & Controls</h3>
+      <div className="flex items-center gap-2">
+        
+        <input
+        type="text"
+        placeholder="Type or paste text here"
+        // value="Password"
+        className="flex-1 px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-300 bg-gray-700"
+        />
+
+        <button
+        className="px-4 py-2 rounded-md shadow-sm border hover:brightness-95 active:scale-95 transition"
+        title="Copy to clipboard"
+        >Copy</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="mt-4 text-gray-800">
+
+        <label className="block text-sm font-medium mb-1">Length: {length}</label>
+        <input
+        type="range"
+        min={1}
+        max={100}
+        className="w-full"/>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className="mt-4 flex items-center gap-6">
+
+        <label className="inline-flex items-center gap-2 text-gray-800">
+        <input
+        type="checkbox"
+        className="w-4 h-4"/>
+        <span>Character</span>
+        </label>
+
+        <label className="inline-flex items-center gap-2 text-gray-800">
+        <input
+        type="checkbox"
+        className="w-4 h-4"/>
+        <span>Number</span>
+        </label>
+
+      </div>
+
+    </div>   
   )
 }
 
