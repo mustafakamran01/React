90706c465e5aaf5c5d591a0555a4543cb6981726
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-18 13:01:40 +0530

    Password generated successfully

--- 05_passwordGenerator/src/App.jsx
+++ 05_passwordGenerator/src/App.jsx
@@ -103,6 +103,30 @@ import './App.css'
 
 function App() {
 
+  const [password, setPassword] = useState("")
+  const [length, setLength] = useState(8)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+
+  const generatePassword = useCallback( () => {
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    let pass = ""
+
+    if (charAllowed) str += "`~!@#$%^&*()"
+    if (numberAllowed) str += "1234567890"
+
+    for (let i = 1; i <= length; i++) {
+      let randomChar = Math.floor(Math.random() * str.length)
+      pass += str.charAt(randomChar)
+    }
+
+    setPassword(pass)
+
+  }, [length, charAllowed, numberAllowed, setPassword])
+
+  useEffect( () => {
+    generatePassword();
+  }, [length, charAllowed, numberAllowed, setPassword])
 
   return(
     <div className="max-w-xl mx-auto p-9 bg-white rounded-2xl shadow-lg justify-center inset-x-0">
@@ -113,23 +137,26 @@ function App() {
         type="text"
         placeholder='password'
         readOnly
+        value={password}
         className="flex-1 px-10 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-amber-800 bg-amber-100 text-black"
         />
 
         <button
         className="px-4 py-2 rounded-md shadow-sm border hover:brightness-95 active:scale-95 transition"
-        title="Copy to clipboard"
+        title="Copy to Clipboard"
         >Copy</button>
         
       </div>
 
       <div className="mt-4 text-rose-500">
 
-        <label className="block text-sm font-medium mb-1 cursor-pointer">LENGTH : {length}</label>
+        <label title='Password Length' className="block text-sm font-medium mb-1 cursor-pointer">LENGTH : {length}</label>
         <input
         type="range"
         min={1}
         max={100}
+        value={length}
+        onChange={ (e) => setLength(e.target.value)}
         className="w-full"/>
 
       </div>
@@ -139,6 +166,7 @@ function App() {
         <label className="inline-flex items-center gap-2 text-stone-900">
         <input
         type="checkbox"
+        onChange={ () => setCharAllowed( (prev) => !prev)}
         className="w-4 h-4"/>
         <span>Character</span>
         </label>
@@ -146,6 +174,7 @@ function App() {
         <label className="inline-flex items-center gap-2 text-stone-900">
         <input
         type="checkbox"
+        onChange={ () => setNumberAllowed( (prev) => !prev)}
         className="w-4 h-4"/>
         <span>Number</span>
         </label>
