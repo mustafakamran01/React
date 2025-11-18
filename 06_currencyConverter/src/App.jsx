4b1e242a26516883fb7920523d4d4300cb08e283
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-18 13:17:44 +0530

    Changing background image

--- 06_currencyConverter/src/App.jsx
+++ 06_currencyConverter/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
     <div
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{
-        backgroundImage: `url('https://images.pexels.com/photos/5980862/pexels-photo-5980862.jpeg')`,
+        backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
       }}>
 
       <div className="w-full">
@@ -58,7 +58,7 @@ function App() {
 
               <button
                 type="button"
-                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"                           
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md bg-blue-600 text-white px-2 py-0.5"                           
                 onClick={swap}
               >Swap
               </button>
