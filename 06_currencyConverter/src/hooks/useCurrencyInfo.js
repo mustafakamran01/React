0aca0424bbc37eeeb75a105480fefaef7e14c395
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-18 15:00:06 +0530

    Creating custom hook

--- 06_currencyConverter/src/hooks/useCurrencyInfo.js
+++ 06_currencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,5 +1,8 @@
 // import { useEffect, useState } from "react"
 
+import { useEffect } from "react"
+import { useState } from "react"
+
 // function useCurrencyInfo(currency) {
 //     const [data, setData] = useState({})
 
@@ -14,3 +17,17 @@
 
 // export default useCurrencyInfo;
 
+const useCurrencyInfo = (currency) => {
+
+    const [data, setData] = useState({})
+
+    useEffect( () => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        .then( (res) => res.json())
+        .then( (res) => setData(res[currency]))
+    }, [currency])
+
+    return data;
+}
+
+export default useCurrencyInfo;
