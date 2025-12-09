239f59cae207436af66116abcba2fdea19100450
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-09 15:26:17 +0530

    Creating multiple components

--- 07_reactRouter/src/App.jsx
+++ 07_reactRouter/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-400 p-4'>React router</h1>
+      
     </>
   )
 }
