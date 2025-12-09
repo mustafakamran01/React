d59bbe72d3cf659e2463d8290e9fcdcbf65678dc
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-09 16:44:55 +0530

    using loader to fetch data from api

--- 07_reactRouter/src/components/Github/Github.jsx
+++ 07_reactRouter/src/components/Github/Github.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router'
 
 function Github() {
 
-    const [data, setData] = useState([])
+    const data = useLoaderData()
 
-    useEffect( () => {
-        fetch('https://api.github.com/users/hiteshchoudhary')
-        .then( (res) => res.json())
-        .then( (res) => setData(res))
-    }, [])
+    // const [data, setData] = useState([])
+
+    // useEffect( () => {
+    //     fetch('https://api.github.com/users/hiteshchoudhary')
+    //     .then( (res) => res.json())
+    //     .then( (res) => setData(res))
+    // }, [])
 
     return (
         <div 
@@ -19,3 +22,8 @@ function Github() {
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    return response.json();
+}
