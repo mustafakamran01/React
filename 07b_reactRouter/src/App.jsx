afa2c86a4574441cc2d3f7e737a1b1c9ef4ee9f3
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-11 12:21:01 +0530

    Creating components

--- 07b_reactRouter/src/App.jsx
+++ 07b_reactRouter/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import {Outlet} from 'react-router'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h2 className='bg-pink-600 text-white text-3xl'>React router</h2>
+      <Header />
+      <Outlet />
+      <Footer />
     </>
   )
 }
