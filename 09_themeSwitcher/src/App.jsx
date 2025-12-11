c0d30652ae2a47b7a821ea4f164ec7ee8a1986ec
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-11 08:28:56 +0530

    Ading CSS to App.jsx file

--- 09_themeSwitcher/src/App.jsx
+++ 09_themeSwitcher/src/App.jsx
@@ -7,9 +7,19 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h2 className='bg-pink-600 text-white p-4 text-3xl'>Chai aur context API</h2>
-    </>
+    
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          {/* themeBtn  */}
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">
+          {/* Card */}
+        </div>
+      </div>
+    </div>
+
   )
 }
 
