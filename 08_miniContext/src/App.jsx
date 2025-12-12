da8dc742e2965b583d9454e7bd962884e3788b25
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-12 18:30:14 +0530

    Setting up project 8

--- 08_miniContext/src/App.jsx
+++ 08_miniContext/src/App.jsx
@@ -1,16 +1,9 @@
 import './App.css'
-import Login from './components/Login'
-import Profile from './components/Profile'
-import UserContextProvider from './context/UserContextProvider'
 
 function App() {
 
   return (
-    <UserContextProvider>
-      <h1>React with Chai</h1>
-      <Login />
-      <Profile />
-    </UserContextProvider>
+    
   )
 }
 
