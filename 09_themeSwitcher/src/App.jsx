6512eefc5938f8dc93b75a08a4c580c3c3742237
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-13 12:08:50 +0530

    Configuring tailwind

--- 09_themeSwitcher/src/App.jsx
+++ 09_themeSwitcher/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
 
@@ -17,8 +19,8 @@ function App() {
   // Actual change in theme
 
   useEffect( () => {
-    const htmlEle = document.querySelector('html').classList.remove('light', 'dark')
-    htmlEle.classList.add(themeMode)
+    document.querySelector('html').classList.remove('light', 'dark')
+    document.querySelector('html').classList.add(themeMode)
   }, [themeMode])
 
   return (
@@ -27,11 +29,11 @@ function App() {
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
             <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-              {/* themeBtn */}
+              <ThemeBtn />
             </div>
 
             <div className="w-full max-w-sm mx-auto">
-              {/* card */}
+              <Card />
             </div>
         </div>
       </div>
