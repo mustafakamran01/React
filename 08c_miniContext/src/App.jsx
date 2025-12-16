a9906aa08c35f948b23cdfe29199210ff220663a
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-16 09:00:39 +0530

    Accumulating all components to App.jsx

--- 08c_miniContext/src/App.jsx
+++ 08c_miniContext/src/App.jsx
@@ -1,12 +1,16 @@
 
 import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './contexts/UserContext'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <UserContextProvider>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
 
