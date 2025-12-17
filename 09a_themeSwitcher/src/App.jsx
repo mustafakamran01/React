9d501fc4a2dc1c6dafd4149d00ab0975416e65ab
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-17 12:21:31 +0530

    Creating components

--- 09a_themeSwitcher/src/App.jsx
+++ 09a_themeSwitcher/src/App.jsx
@@ -1,12 +1,43 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/ThemeContexts'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
 
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  // Actual way to change mode in HTMl 
+
+  useEffect( () => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+
   return (
-    <>
-      <h2 className='bg-blue-800 text-3xl text-white p-5'>React with context API</h2>
-    </>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+               <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+              <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
