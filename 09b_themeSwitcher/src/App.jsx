590152f7075ef4688541e722d54cfed101dcab40
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-18 16:44:08 +0530

    Accumulating all components to App.jsx

--- 09b_themeSwitcher/src/App.jsx
+++ 09b_themeSwitcher/src/App.jsx
@@ -2,6 +2,8 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './contexts/ThemeContext'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
 
@@ -27,11 +29,11 @@ function App() {
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-             {/* themeBtn */}
+             <ThemeBtn />
           </div>
 
           <div className="w-full max-w-sm mx-auto">
-             {/* card */}
+             <Card />
           </div>
         </div>
       </div>
