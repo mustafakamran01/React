135d17122bff458221325f99818aec020d7e91bd
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-21 12:29:20 +0530

    Adding functionality to App.jsx

--- 09c_themeSwitcher/src/App.jsx
+++ 09c_themeSwitcher/src/App.jsx
@@ -1,22 +1,42 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/ThemeContext'
 
-function App() {
+function App() { 
+
+  const [theme, setTheme] = useState("light")
+
+  const themeMode = () => {
+    if (themeMode == "light") {
+      themeMode = "dark"
+    } else {
+      themeMode = "light"
+    }
+  }
+
+  // Actual way of changing theme in HTML
+
+  useEffect( () => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  })
 
   return (
     
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          {/* themeBtn */}
-        </div>
+    <ThemeProvider value={{theme, themeMode}}>                                                                                                                                                                                                                   
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* themeBtn */}
+          </div>
 
-        <div className="w-full max-w-sm mx-auto">
-          {/* card */}
+          <div className="w-full max-w-sm mx-auto">
+            {/* card */}
+          </div>
         </div>
       </div>
-    </div>
-
+    </ThemeProvider>
   )
 }
 
