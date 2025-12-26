1fb30e1cc99ce3d327c4c8fdfc93619d298b1fd1
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-26 18:54:36 +0530

    Structuring project "Password Generator"

---
+++ 05a_passwordGenerator/src/App.jsx
@@ -0,0 +1,58 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="max-w-xl mx-auto p-9 bg-white rounded-2xl shadow-lg justify-center inset-x-0">
+      <h3 className="text-2xl font-semibold mb-4 text-black">Password Generator</h3>
+      <div className="flex items-center gap-1">
+        
+        <input
+        type="text"
+        placeholder='password'
+        readOnly
+        className="flex-1 px-10 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-amber-800 bg-amber-100 text-black"
+        />
+
+        <button
+        className="px-4 py-2 rounded-md shadow-sm border hover:brightness-95 active:scale-95 transition"
+        title="Copy to clipboard"
+        >Copy</button>
+        
+      </div>
+
+      <div className="mt-4 text-rose-500">
+
+        <label className="block text-sm font-medium mb-1 cursor-pointer">LENGTH : {length}</label>
+        <input
+        type="range"
+        min={1}
+        max={100}
+        className="w-full"/>
+
+      </div>
+
+      <div className="mt-4 flex items-center gap-6">
+
+        <label className="inline-flex items-center gap-2 text-stone-900">
+        <input
+        type="checkbox"
+        className="w-4 h-4"/>
+        <span>Character</span>
+        </label>
+
+        <label className="inline-flex items-center gap-2 text-stone-900">
+        <input
+        type="checkbox"
+        className="w-4 h-4"/>
+        <span>Number</span>
+        </label>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
