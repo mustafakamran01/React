e744e62adaee314e286426173611a79396570eb3
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-26 23:51:48 +0530

    Structuring project "Currency Converter"

---
+++ 06a_currencyConverter/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='bg-amber-700 text-white text-3xl p-5 '>Currency converter</h2>
+    </>
+  )
+}
+
+export default App
