744c720cd4db9a8322bdc505e686d90b78db8bc4
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-30 10:37:01 +0530

    Structuring project "React-router"

---
+++ 07c_reactRouter/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className= ' bg-amber-800 text-white text-3xl p-5'>React router</h2>
+    </>
+  )
+}
+
+export default App
