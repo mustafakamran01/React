e3d617d9db5e08d50ac85ecece658090332fabf1
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-30 13:41:29 +0530

    Arranging all the codes

--- 07c_reactRouter/src/Components/Github/Github.jsx
+++ 07c_reactRouter/src/Components/Github/Github.jsx
@@ -30,16 +30,6 @@ function Github() {
                 </li>
                 <li>
                     <NavLink
-                        to="hitesh"
-                        className={({isActive}) =>
-                            `block py-2 pr-4 pl-3 duration-200 border-b ${isActive ? "text-orange-700" : "text-gray-700"} border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                        }
-                    >
-                        Hitesh Choudhary
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
                         to="shahnawaz"
                         className={({isActive}) =>
                             `block py-2 pr-4 pl-3 duration-200 border-b ${isActive ? "text-orange-700" : "text-gray-700"} border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
