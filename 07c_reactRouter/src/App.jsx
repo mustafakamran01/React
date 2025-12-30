f0b36b97ec7e4bdd733c27d8499f14cf98515f46
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-30 10:44:16 +0530

    Installing react router

--- 07c_reactRouter/src/App.jsx
+++ 07c_reactRouter/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h2 className= ' bg-amber-800 text-white text-3xl p-5'>React router</h2>
+      {/* <h2 className= ' bg-amber-800 text-white text-3xl p-5'>React router</h2> */}
     </>
   )
 }
